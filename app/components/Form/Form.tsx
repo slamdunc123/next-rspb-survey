7debc0b329dd8cd4671c768660ada75fcb1fbe9c
duncanlaidlaw <slamdunc123@gmail.com>
2023-07-22 13:26:11 +0100

    installed mui, add mui styling to form

--- app/components/Form/Form.tsx
+++ app/components/Form/Form.tsx
@@ -1,28 +1,31 @@
 'use client';
 
 import React, { FormEvent, ChangeEventHandler, useState } from 'react';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
+import Box from '@mui/material/Box';
 
 const Form = () => {
 	const initialFormData = {
 		fieldNo: '',
-    startTime: '',
-    curlew: '',
-    lapwing: '',
-    oystercatcherPairs: '',
-    oystercatcherSingles: '',
-    snipeDrumChip: '',
-    snipeTotal: '',
-    landUseCode: '',
-    grazed: '',
-    vegHeight: '',
-    fieldWetness: '',
-    standingWater: '',
-    du: '',
-    su: '',
-    mr: '',
-    mn: '',
-    foxCrow: '',
-
+		startTime: '',
+		curlew: '',
+		lapwing: '',
+		oystercatcherPairs: '',
+		oystercatcherSingles: '',
+		snipeDrumChip: '',
+		snipeTotal: '',
+		landUseCode: '',
+		grazed: '',
+		vegHeight: '',
+		fieldWetness: '',
+		standingWater: '',
+		du: '',
+		su: '',
+		mr: '',
+		mn: '',
+		foxCrow: '',
 	};
 
 	const [formData, setFormData] = useState(initialFormData);
@@ -36,141 +39,232 @@ const Form = () => {
 		console.log(formData);
 	};
 
-  const handleReset = () => {
-    setFormData(initialFormData)
-  }
+	const handleReset = () => {
+		setFormData(initialFormData);
+	};
 	return (
 		<>
 			<div>Form</div>
 			<form onSubmit={(e) => handleSubmit(e)}>
-				<input
+				<TextField
+					id='outlined-basic'
+					label='Field No'
+					variant='outlined'
 					type='text'
 					name='fieldNo'
-          value={formData.fieldNo}
-          placeholder='Field No'
-					onChange={(e) => handleOnChange(e)}
+					value={formData.fieldNo}
+					onChange={handleOnChange}
+					size='small'
+					margin='normal'
 				/>
-				<input
+				<TextField
+					id='outlined-basic'
+					label='Start Time'
+					variant='outlined'
 					type='text'
 					name='startTime'
-          value={formData.startTime}
-          placeholder='Start Time'
-					onChange={(e) => handleOnChange(e)}
+					value={formData.startTime}
+					onChange={handleOnChange}
+					size='small'
+					margin='normal'
 				/>
-				<input
+				<TextField
+					id='outlined-basic'
+					label='Curlew'
+					variant='outlined'
 					type='text'
 					name='curlew'
-          value={formData.curlew}
-          placeholder='Curlew'
-					onChange={(e) => handleOnChange(e)}
+					value={formData.curlew}
+					onChange={handleOnChange}
+					size='small'
+					margin='normal'
 				/>
-				<input
+				<TextField
+					id='outlined-basic'
+					label='Lapwing'
+					variant='outlined'
 					type='text'
 					name='lapwing'
-          value={formData.lapwing}
-          placeholder='Lapwing'
-					onChange={(e) => handleOnChange(e)}
+					value={formData.lapwing}
+					onChange={handleOnChange}
+					size='small'
+					margin='normal'
 				/>
-				<input
+				<TextField
+					id='outlined-basic'
+					label='Oystercatcher Pairs'
+					variant='outlined'
 					type='text'
 					name='oystercatcherPairs'
-          value={formData.oystercatcherPairs}
-          placeholder='Oystercatcher Pairs'
-					onChange={(e) => handleOnChange(e)}
+					value={formData.oystercatcherPairs}
+					onChange={handleOnChange}
+					size='small'
+					margin='normal'
 				/>
-				<input
+				<TextField
+					id='outlined-basic'
+					label='Oystercatcher Singles'
+					variant='outlined'
 					type='text'
 					name='oystercatcherSingles'
-          value={formData.oystercatcherSingles}
-          placeholder='Oystercatcher Singles'
-					onChange={(e) => handleOnChange(e)}
+					value={formData.oystercatcherSingles}
+					onChange={handleOnChange}
+					size='small'
+					margin='normal'
 				/>
-				<input
+				<TextField
+					id='outlined-basic'
+					label='Snipe Drumming / Chipping'
+					variant='outlined'
 					type='text'
 					name='snipeDrumChip'
-          value={formData.snipeDrumChip}
-          placeholder='Snipe Drumming / Chipping'
-					onChange={(e) => handleOnChange(e)}
+					value={formData.snipeDrumChip}
+					onChange={handleOnChange}
+					size='small'
+					margin='normal'
 				/>
-				<input
+				<TextField
+					id='outlined-basic'
+					label='Snipe Total'
+					variant='outlined'
 					type='text'
 					name='snipeTotal'
-          value={formData.snipeTotal}
-          placeholder='Snipe Total'
-					onChange={(e) => handleOnChange(e)}
+					value={formData.snipeTotal}
+					onChange={handleOnChange}
+					size='small'
+					margin='normal'
 				/>
-				<input
+				<TextField
+					id='outlined-basic'
+					label='Land Use Code'
+					variant='outlined'
 					type='text'
 					name='landUseCode'
-          value={formData.landUseCode}
-          placeholder='Land Use Code'
-					onChange={(e) => handleOnChange(e)}
+					value={formData.landUseCode}
+					onChange={handleOnChange}
+					size='small'
+					margin='normal'
 				/>
-				<input
+				<TextField
+					id='outlined-basic'
+					label='Grazed'
+					variant='outlined'
 					type='text'
 					name='grazed'
-          value={formData.grazed}
-          placeholder='Grazed'
-					onChange={(e) => handleOnChange(e)}
+					value={formData.grazed}
+					onChange={handleOnChange}
+					size='small'
+					margin='normal'
 				/>
-				<input
+				<TextField
+					id='outlined-basic'
+					label='Veg Height'
+					variant='outlined'
 					type='text'
 					name='vegHeight'
-          value={formData.vegHeight}
-          placeholder='Veg Height'
-					onChange={(e) => handleOnChange(e)}
+					value={formData.vegHeight}
+					onChange={handleOnChange}
+					size='small'
+					margin='normal'
 				/>
-				<input
+				<TextField
+					id='outlined-basic'
+					label='Field Wetness'
+					variant='outlined'
 					type='text'
 					name='fieldWetness'
-          value={formData.fieldWetness}
-          placeholder='Field Wetness'
-					onChange={(e) => handleOnChange(e)}
+					value={formData.fieldWetness}
+					onChange={handleOnChange}
+					size='small'
+					margin='normal'
 				/>
-				<input
+				<TextField
+					id='outlined-basic'
+					label='Standing Water'
+					variant='outlined'
 					type='text'
 					name='standingWater'
-          value={formData.standingWater}
-          placeholder='Standing Water'
-					onChange={(e) => handleOnChange(e)}
+					value={formData.standingWater}
+					onChange={handleOnChange}
+					size='small'
+					margin='normal'
 				/>
-				<input
+				<TextField
+					id='outlined-basic'
+					label='DU'
+					variant='outlined'
 					type='text'
 					name='du'
-          value={formData.du}
-          placeholder='DU'
-					onChange={(e) => handleOnChange(e)}
+					value={formData.du}
+					onChange={handleOnChange}
+					size='small'
+					margin='normal'
 				/>
-				<input
+				<TextField
+					id='outlined-basic'
+					label='SU'
+					variant='outlined'
 					type='text'
 					name='su'
-          value={formData.su}
-          placeholder='SU'
-					onChange={(e) => handleOnChange(e)}
+					value={formData.su}
+					onChange={handleOnChange}
+					size='small'
+					margin='normal'
 				/>
-				<input
+				<TextField
+					id='outlined-basic'
+					label='MR'
+					variant='outlined'
 					type='text'
 					name='mr'
-          value={formData.mr}
-          placeholder='MR'
-					onChange={(e) => handleOnChange(e)}
+					value={formData.mr}
+					onChange={handleOnChange}
+					size='small'
+					margin='normal'
 				/>
-				<input
+				<TextField
+					id='outlined-basic'
+					label='MN'
+					variant='outlined'
 					type='text'
 					name='mn'
-          value={formData.mn}
-          placeholder='MN'
-					onChange={(e) => handleOnChange(e)}
+					value={formData.mn}
+					onChange={handleOnChange}
+					size='small'
+					margin='normal'
 				/>
-				<input
+				<TextField
+					id='outlined-basic'
+					label='Foxes / Crows'
+					variant='outlined'
 					type='text'
 					name='foxCrow'
-          value={formData.foxCrow}
-          placeholder='Foxes / Crows'
-					onChange={(e) => handleOnChange(e)}
+					value={formData.foxCrow}
+					onChange={handleOnChange}
+					size='small'
+					margin='normal'
 				/>
-				<button>Submit</button>
-				<button onClick={handleReset}>Reset</button>
+				<ButtonGroup size='small' sx={{ marginTop: 2 }}>
+					<Box mr={2}>
+						<Button
+							variant='contained'
+							type='submit'
+							value='Submit'
+						>
+							Submit
+						</Button>
+					</Box>
+					<Box>
+						<Button
+							variant='contained'
+							type='button'
+							value='Reset'
+							onClick={handleReset}
+						>
+							Reset
+						</Button>
+					</Box>
+				</ButtonGroup>
 			</form>
 		</>
 	);
